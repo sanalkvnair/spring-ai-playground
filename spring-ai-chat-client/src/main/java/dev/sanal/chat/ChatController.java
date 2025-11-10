0cf015835f28af599354d3fb51b01cf263b00206
Sanal Nair <sanal.nair4@ingka.com>
2025-11-10 17:04:03 +0100

    setup chat client with openai

---
+++ spring-ai-chat-client/src/main/java/dev/sanal/chat/ChatController.java
@@ -0,0 +1,23 @@
+package dev.sanal.chat;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class ChatController {
+
+    private final ChatClient chatClient;
+
+    public ChatController(ChatClient.Builder chatClientBuilder) {
+        this.chatClient = chatClientBuilder.build();
+    }
+
+    @GetMapping("/ai")
+    String generation(@RequestParam("userInput") String userInput) {
+        return this.chatClient.prompt().user(userInput).call().content();
+    }
+}
